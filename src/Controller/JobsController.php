6913182dbac35dfbc1cc1db891b78e402e404f66
paulius3881 <arbapaulius1998@gmail.com>
2019-06-24 11:48:04 +0300

    Created menu, created table with jobs

--- src/Controller/JobsController.php
+++ src/Controller/JobsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +13,10 @@ class JobsController extends AbstractController
      */
     public function index()
     {
-       
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
         return $this->render('jobs/index.html.twig', [
             'controller_name' => 'JobsController',
+            'jobs' => $jobs,
         ]);
     }
 }
